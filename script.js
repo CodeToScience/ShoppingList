714751e743c00ae6c453eb36ddea98d415998aa4
CodeToScience <55003801+CodeToScience@users.noreply.github.com>
2019-12-02 00:39:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,33 +61,3 @@ function removeParent(evt) {
   evt.target.parentNode.remove();
 }
 
-// var button = document.getElementById("enter");
-// var input = document.getElementById("userinput");
-// var ul = document.querySelector("ul");
-
-// function inputLength() {
-// 	return input.value.length;
-// }
-
-// function createListElement() {
-// 	var li = document.createElement("li");
-// 	li.appendChild(document.createTextNode(input.value));
-// 	ul.appendChild(li);
-// 	input.value = "";
-// }
-
-// function addListAfterClick() {
-// 	if (inputLength() > 0) {
-// 		createListElement();
-// 	}
-// }
-
-// function addListAfterKeypress(event) {
-// 	if (inputLength() > 0 && event.keyCode === 13) {
-// 		createListElement();
-// 	}
-// }
-
-// button.addEventListener("click", addListAfterClick);
-
-// input.addEventListener("keypress", addListAfterKeypress);
